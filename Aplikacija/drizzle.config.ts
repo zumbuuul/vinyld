d2c9b6fb840bb5d2910deec4c7ba52dc057cb66c
lazar <krstiic.lazar@gmail.com>
2026-05-28 15:39:16 +0200

    chore: updated db connection string

--- Aplikacija/drizzle.config.ts
+++ Aplikacija/drizzle.config.ts
@@ -1,11 +1,11 @@
-import 'dotenv/config'
-import {defineConfig} from "drizzle-kit"
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    out: './drizzle',
-    dialect: 'postgresql',
-    schema: './src/db/schema',
-    dbCredentials: {
-        url: process.env.DB_URL!
-    }
-})
+  out: "./drizzle",
+  dialect: "postgresql",
+  schema: "./src/db/schema.ts",
+  dbCredentials: {
+    url: process.env.DB_URL!,
+  },
+});
